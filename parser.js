fe41ef39af9bf5362eacbcffe0097803925ab0ef
joesturge <joe.mike.sturge@gmail.com>
2022-02-09 20:56:26 +0000

    printing paths

--- parser.js
+++ parser.js
@@ -1,37 +1,34 @@
-const { duplex } = require("event-stream");
+const { duplex, stringify } = require("event-stream");
 const { through, split } = require("event-stream");
 const { on } = require("events");
 const fs = require("fs");
 const moo = require("moo");
+const { type } = require("os");
 
 const comment = {
   match: /\s*#.*/,
   value: (s) => s.replace("#", "").trim(),
 };
 const blank = {
-  match: /[\t ]+/
+  match: /[\t ]+/,
 };
 
 const JominiStream = () => {
   const lexer = moo.states({
     descend: {
+      header: /SAV.+/,
       ascend: {
         match: /\s*}/,
-        pop: 1
+        pop: 1,
       },
       objectStart: {
         match: /(?=\s*[^=]+=)/,
         push: "object",
       },
       arrayStart: {
-        match: /(?=\s*(?:"[^={}]+"|[^={}\s]+))/,
+        match: /(?=\s*(?:"[^={}]+"|[^={}\s]+|{))/,
         push: "array",
       },
-      arrayStart: {
-        match: /(?=\s*(?:"[^={}]+"|[^={}\s]+))/,
-        push: "array",
-      },
-      comment,
     },
     object: {
       key: {
@@ -40,33 +37,38 @@ const JominiStream = () => {
       },
       value: {
         match: /=\s*(?:"[^={}]+"|[^={}\s]+)/,
-        value: (s) => s.replace("=", "").trim(),
+        value: (s) => s.replace(/["=]/g, "").trim(),
       },
       descend: {
-        match: /=\s*{+/,
+        match: /=\s*{/,
         push: "descend",
       },
       objectEnd: {
         match: /(?=\s*})/,
-        pop: 1
+        pop: 1,
       },
       comment,
-      blank
+      blank,
     },
     array: {
-      value: {
+      arrayValue: {
         match: /\s*(?:"[^={}]+"|[^={}\s]+)/,
-        value: (s) => s.replace("=", "").trim(),
+        value: (s) => s.replace(/"/g, "").trim(),
+      },
+      descend: {
+        match: /\s*{/,
+        push: "descend",
       },
       arrayEnd: {
         match: /(?=\s*})/,
-        pop: 1
-      }
-    }
+        pop: 1,
+      },
+      comment,
+      blank,
+    },
   });
 
   var lexerLine = 1;
-
   const path = [];
 
   const splitterStream = split();
@@ -77,7 +79,30 @@ const JominiStream = () => {
       lexer.reset(data, { ...lexer.save(), line: lexerLine++, col: 0 });
 
       for (let token of lexer) {
-        this.emit("data", token);
+        switch (token.type) {
+          case "key":
+            path.push(token.value);
+            break;
+          case "value":
+            path.pop();
+            this.emit("data", { path, value: token.value });
+            break;
+          case "arrayStart":
+            path.push("*");
+            break;
+          case "arrayEnd":
+            path.pop();
+            path.pop();
+            break;
+          case "objectEnd":
+            if(path.at(-1) !== "*") {
+              path.pop();
+            }
+            break;
+          case "arrayValue":
+            this.emit("data", { path, value: token.value });
+          default:
+        }
       }
 
       this.resume();
@@ -89,4 +114,5 @@ const JominiStream = () => {
 
 fs.createReadStream("test/data/sample.txt", { encoding: "utf-8" })
   .pipe(JominiStream(["k_papal_state"]))
-  .on("data", console.log);
+  .pipe(stringify())
+  .pipe(fs.createWriteStream("output.jsonl"));
