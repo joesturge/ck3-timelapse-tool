cfca3a85aec79a5a038b4f387c1920277713f3a8
joesturge <joe.mike.sturge@gmail.com>
2022-02-09 14:26:30 +0000

    added custom parser using moo and streams

---
+++ parser.js
@@ -0,0 +1,60 @@
+const { duplex } = require("event-stream");
+const { through, split } = require("event-stream");
+const fs = require("fs");
+const moo = require("moo");
+
+const JominiStream = () => {
+  const lexer = moo.states({
+    object: {
+      key: {
+        match: /[^=]+=/,
+        value: (s) => s.replace(/=/, "").trim(),
+        push: "field",
+      },
+      comment: /#.+/,
+      arrayItem: {
+        match: /\s*(?:"[^={}]+"|[^={}\s]+)\s*/,
+        value: (s) => s.replace(/=/, "").trim(),
+      },
+      objectEnd: {
+        match: /s*}\s*/,
+        pop: 1,
+      },
+      ignore: /./
+    },
+    field: {
+      string: {
+        match: /(?<==)\s*(?:"[^={}]+"|[^={}\s]+)/,
+        value: (s) => s.trim().replace(/"/g, ""),
+        pop: 1,
+      },
+      objectStart: {
+        match: /(?<==)\s*{\s*/,
+        push: "object",
+      },
+    }
+  });
+
+  const splitterStream = split();
+  const lexingStream = splitterStream.pipe(
+    through(
+      function write(data) {
+        this.pause();
+        lexer.reset(data);
+        for (let here of lexer) {
+          this.emit("data", { value: here.value, type: here.type });
+        }
+        this.resume();
+      },
+      function end(data) {
+        console.log("end");
+      }
+    )
+  );
+
+  return duplex(splitterStream, lexingStream);
+};
+
+fs.createReadStream("test/data/sample.txt", { encoding: "utf-8" })
+  .pipe(JominiStream(["k_papal_state"]))
+  .on("data", (data) => console.log("data", data));
